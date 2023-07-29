717ebe0bfa7c22c4f2405664dec8603700d4bb96
Oscar Hsu <vm3y3rmp40719@gmail.com>
2023-07-29 23:39:36 +0800

    Update all

--- js/main.js
+++ js/main.js
@@ -10,55 +10,105 @@ $(document).ready(() => {
      */
     const project_1 = {
         'title': {
-            'en': '<a href="https://bit.ly/35GzBmc" target="_blank">Chord learning and adversarial framework for symbolic music generation</a>', 
-            'zh_tw': '<a href="https://bit.ly/35GzBmc" target="_blank">基於生成對抗網路的古箏自動編曲系統</a>'
+            'en': '<a href="https://docs.google.com/presentation/d/1797idRRmgeD2JnUslGbflmGUO2X5lMY6/edit?usp=drive_link&ouid=101248488395326982475&rtpof=true&sd=true" target="_blank">Chord learning and adversarial framework for symbolic music generation</a>', 
+            'zh_tw': '<a href="https://docs.google.com/presentation/d/1797idRRmgeD2JnUslGbflmGUO2X5lMY6/edit?usp=drive_link&ouid=101248488395326982475&rtpof=true&sd=true" target="_blank">基於生成對抗網路的古箏自動編曲系統</a>'
         }, 
         'main': {
             'en': [
-                'Proposed a chord learning framework for multi-track symbolic music generation based on VAE and GAN, and implemented with <a href="https://pytorch.org/" target="_blank">PyTorch</a>.', 
-                'Improved VAE to extract chords more easily.', 
-                'Improved <a href="https://arxiv.org/abs/1704.00028" target="_blank">WGAN-GP</a> to solve mode collapse problem, vanishing gradient problem, and bad results of some audio tracks.', 
-                'Proposed three distinct loss functions, <em>element-wise</em>, <em>discriminator dominant</em>, and <em>hybrid</em>.'
+                'Proposed a chord learning framework for multi-track symbolic music generation based on VAE and GAN.', 
+                'Introduced <a href="https://arxiv.org/abs/1704.00028" target="_blank">WGAN-GP</a> to solve mode collapse and vanishing gradient problem.', 
             ], 
             'zh_tw': [
-                '基於變分自動編碼器 (VAE) 和生成對抗網路 (GAN)，提出一個生成多音軌 (multi-track) 符號音樂 (symbolic music) 的框架，並通過<a href="https://pytorch.org/" target="_blank">PyTorch</a>實現。', 
-                '改進VAE，使得和弦更容易解析。', 
-                '改進WGAN-GP，解決模式崩壞 (mode collapse)、梯度消失 (vanishing gradient) 和部分音軌效果較差的問題。', 
-                '提出三種損失函數 (loss function)：<em>element-wise</em>、<em>discriminator dominant</em>和<em>hybrid</em>。'
+                '基於變分自動編碼器 (VAE) 和生成對抗網路 (GAN)，提出一個生成多音軌符號 (symbolic) 音樂的框架。', 
+                '引入 <a href="https://arxiv.org/abs/1704.00028" target="_blank">WGAN-GP</a> 解決模式崩壞 (mode collapse) 以及梯度消失 (vanishing gradient) 問題。', 
             ]
+        },
+        'period': {
+            'en': '04 2018 - 05 2019',
+            'zh_tw': '04 2018 - 05 2019',
         }, 
         'location': {
-            'en': '<em>Hangzhou, China</em>', 
-            'zh_tw': '<em>杭州, 中國</em>'
+            'en': 'Hangzhou, China', 
+            'zh_tw': '杭州, 中國'
         }
     };
     const project_2 = {
         'title': {
-            'en': '<a href="https://bit.ly/3IzE3BU" target="_blank">Voice Conversion Based on Generative Adversarial Networks</a>', 
-            'zh_tw': '<a href="https://bit.ly/3IzE3BU" target="_blank">基於生成對抗網路的語音轉換研究</a>' 
+            'en': '<a href="https://drive.google.com/file/d/1736XHtaeT58FL_gbOa9XjlyA6xAXDkg2/view?usp=drive_link" target="_blank">Voice Conversion Based on Generative Adversarial Networks</a>', 
+            'zh_tw': '<a href="https://drive.google.com/file/d/1736XHtaeT58FL_gbOa9XjlyA6xAXDkg2/view?usp=drive_link" target="_blank">基於生成對抗網路的語音轉換研究</a>' 
         }, 
         'main': {
             'en': [
                 'Improved <a href="https://arxiv.org/abs/1907.12279" target="_blank">StarGAN-VC2</a> based on multi-speaker non-parallel corpus.', 
-                'Introduced <a href="https://arxiv.org/abs/1712.01026" target="_blank">WGAN-div</a> to resolve k-Lipschitz constraint of <a href="https://arxiv.org/abs/1701.07875" target="_blank">WGAN</a>.', 
-                'Provided new generator network by replacing <a href="https://arxiv.org/abs/1610.07629" target="_blank">cIN</a> layer with trainable <a href="https://arxiv.org/abs/1703.06868" target="_blank">AdaIN</a> layer for better performance.', 
-                'Introduced neural vocoder, <a href="https://arxiv.org/abs/1811.00002" target="_blank">WaveGlow</a>, to generate high quality speech from mel-spectrograms.'
+                'Introduced <a href="https://arxiv.org/abs/1712.01026" target="_blank">WGAN-div</a>, <a href="https://arxiv.org/abs/1703.06868" target="_blank">AdaIN</a> layer, and neural vocoder, <a href="https://arxiv.org/abs/1811.00002" target="_blank">WaveGlow</a>, for better generated audio quality.', 
             ], 
             'zh_tw': [
-                '基於多語者 (multi-speaker) 非平行語料庫 (non-parallel corpus)，改進<a href="https://arxiv.org/abs/1907.12279" target="_blank">StarGAN-VC2</a>。', 
-                '引入<a href="https://arxiv.org/abs/1712.01026" target="_blank">WGAN-div</a>解決<a href="https://arxiv.org/abs/1701.07875" target="_blank">WGAN</a>原有的k-Lipschitz限制。', 
-                '通過將生成網路 (generator) 的<a href="https://arxiv.org/abs/1610.07629" target="_blank">cIN</a>層替換成<a href="https://arxiv.org/abs/1703.06868" target="_blank">AdaIN</a>層，提升生成的語音效果。', 
-                '引入神經網路語音合成器 (neural vocoder) <a href="https://arxiv.org/abs/1811.00002" target="_blank">WaveGlow</a>，通過梅爾時頻譜 (mel-spectrograms) 生成較高音質的語音。'
+                '在多語者 (multi-speaker) 非平行語料庫 (non-parallel corpus) 基礎上改進 <a href="https://arxiv.org/abs/1907.12279" target="_blank">StarGAN-VC2</a>。', 
+                '引入 <a href="https://arxiv.org/abs/1712.01026" target="_blank">WGAN-div</a>, <a href="https://arxiv.org/abs/1703.06868" target="_blank">AdaIN</a> 層和 <a href="https://arxiv.org/abs/1811.00002" target="_blank">WaveGlow</a> 神經聲碼器 (neural vocoder)，提升生成的語音品質。', 
             ]
         }, 
+        'period': {
+            'en': '03 2020 - 05 2020',
+            'zh_tw': '03 2020 - 05 2020',
+        }, 
         'location': {
-            'en': '<em>Hangzhou, China</em>', 
-            'zh_tw': '<em>杭州, 中國</em>'
+            'en': 'Hangzhou, China', 
+            'zh_tw': '杭州, 中國'
+        }
+    };
+    const project_3 = {
+        'title': {
+            'en': '<a href="https://drive.google.com/file/d/176xCWH6j0VAuoXLtB_HppxyZN-6ezq9_/view?usp=drive_link" target="_blank">SurrogateFlow: Synthetic Data Generation using Conditional Normalizing Flows</a>', 
+            'zh_tw': '<a href="https://drive.google.com/file/d/176xCWH6j0VAuoXLtB_HppxyZN-6ezq9_/view?usp=drive_link" target="_blank">SurrogateFlow: Synthetic Data Generation using Conditional Normalizing Flows</a>' 
+        }, 
+        'main': {
+            'en': [
+                'Ability to replace original datasets and maintain same data distribution, and also highlight outliers for analysis.', 
+                'Built a browser-server web application to visualize results.', 
+            ], 
+            'zh_tw': [
+                '提出一個條件正規化流 (CNF) 模型，用於替換原始資料集，並生成新的資料集，同時能強調異常值 (outlier)。', 
+                '建立一個瀏覽器-伺服器 (browser-server) 網路應用，用於視覺化結果。'
+            ]
+        }, 
+        'period': {
+            'en': '03 2023 - 05 2023',
+            'zh_tw': '03 2023 - 05 2023',
+        }, 
+        'location': {
+            'en': 'Hsinchu, Taiwan', 
+            'zh_tw': '新竹, 臺灣'
+        }
+    };
+    const project_4 = {
+        'title': {
+            'en': 'Floating image quality compensation algorithm technology', 
+            'zh_tw': '立體浮空影像品質補償演算法技術' 
+        }, 
+        'main': {
+            'en': [
+                'Improved image quality of poorly captured floating images due to hardware tolerance based on CNNs.', 
+                'Ability to train and employ on different devices.', 
+            ], 
+            'zh_tw': [
+                '提出一個基於卷積神經網路 (CNN) 的浮空影像品質補償演算法，用於解決硬體缺陷問題，提升浮空影像的品質。', 
+                '可以在不同的硬體上訓練和部署。', 
+            ]
+        }, 
+        'period': {
+            'en': '06 2023 - 12 2023 (Expected)',
+            'zh_tw': '06 2023 - 12 2023 (Expected)',
+        }, 
+        'location': {
+            'en': 'Hsinchu, Taiwan', 
+            'zh_tw': '新竹, 臺灣'
         }
     };
     const projects = [
         project_1, 
-        project_2
+        project_2, 
+        project_3, 
+        project_4
     ];
 
     /*
@@ -70,12 +120,16 @@ $(document).ready(() => {
             'zh_tw': '中國高校計算機大賽移動應用創新賽'
         }, 
         'prize': {
-            'en': '<em>First Prize</em>', 
-            'zh_tw': '<em>一等獎</em>'
-        },
+            'en': 'First Prize and Most Innovative Award', 
+            'zh_tw': '一等獎、最具創新獎'
+        }, 
+        'period': {
+            'en': '09 2018',
+            'zh_tw': '09 2018',
+        }, 
         'location': {
-            'en': '<em>Hangzhou, China</em>', 
-            'zh_tw': '<em>杭州, 中國</em>'
+            'en': 'Hangzhou, China', 
+            'zh_tw': '杭州, 中國'
         }
     };
     const award_2 = {
@@ -84,12 +138,16 @@ $(document).ready(() => {
             'zh_tw': '第四屆中國"互聯網+"大學生創新創業大賽'
         }, 
         'prize': {
-            'en': '<em>Gold Award</em>', 
-            'zh_tw': '<em>金獎</em>'
-        },
+            'en': 'Gold Award', 
+            'zh_tw': '金獎'
+        }, 
+        'period': {
+            'en': '09 2018',
+            'zh_tw': '09 2018',
+        }, 
         'location': {
-            'en': '<em>Hangzhou, China</em>', 
-            'zh_tw': '<em>杭州, 中國</em>'
+            'en': 'Hangzhou, China', 
+            'zh_tw': '杭州, 中國'
         }
     };
     const award_3 = {
@@ -98,12 +156,16 @@ $(document).ready(() => {
             'zh_tw': '港澳台學生獎學金'
         }, 
         'prize': {
-            'en': '<em>Third Prize</em>', 
-            'zh_tw': '<em>三等獎</em>'
-        },
+            'en': 'Third Prize', 
+            'zh_tw': '三等獎'
+        }, 
+        'period': {
+            'en': '09 2018',
+            'zh_tw': '09 2018',
+        }, 
         'location': {
-            'en': '<em>Hangzhou, China</em>', 
-            'zh_tw': '<em>杭州, 中國</em>'
+            'en': 'Hangzhou, China', 
+            'zh_tw': '杭州, 中國'
         }
     };
     const award_4 = {
@@ -112,12 +174,16 @@ $(document).ready(() => {
             'zh_tw': '國家級大學生創新創業訓練計畫 (SRTP)' 
         }, 
         'prize': {
-            'en': '<em>Excellent</em>', 
-            'zh_tw': '<em>優秀</em>'
-        },
+            'en': 'Excellent', 
+            'zh_tw': '優秀'
+        }, 
+        'period': {
+            'en': '04 2018 - 05 2019',
+            'zh_tw': '04 2018 - 05 2019',
+        }, 
         'location': {
-            'en': '<em>Hangzhou, China</em>', 
-            'zh_tw': '<em>杭州, 中國</em>'
+            'en': 'Hangzhou, China', 
+            'zh_tw': '杭州, 中國'
         }
     };
     const award_5 = {
@@ -126,12 +192,16 @@ $(document).ready(() => {
             'zh_tw': '港澳台學生獎學金'
         }, 
         'prize': {
-            'en': '<em>Second Prize</em>', 
-            'zh_tw': '<em>二等獎</em>'
-        },
+            'en': 'Second Prize', 
+            'zh_tw': '二等獎'
+        }, 
+        'period': {
+            'en': '12 2019',
+            'zh_tw': '12 2019',
+        }, 
         'location': {
-            'en': '<em>Hangzhou, China</em>', 
-            'zh_tw': '<em>杭州, 中國</em>'
+            'en': 'Hangzhou, China', 
+            'zh_tw': '杭州, 中國'
         }
     };
     const awards = [
@@ -147,24 +217,28 @@ $(document).ready(() => {
             'zh_tw': '浙江大學'
         }, 
         'degree': {
-            'en': '<em>Bachelor of Engineering in Computer Science and Technology</em>', 
-            'zh_tw': '<em>計算機科學與技術學士</em>'
+            'en': 'Bachelor of Engineering in Computer Science and Technology', 
+            'zh_tw': '計算機科學與技術學士'
         },
         'remark': {
-            'en': 'Last two years cumulative GPA: 3.30/4.00', 
-            'zh_tw': '最後兩年GPA: 3.30/4.00'
-        },
+            'en': 'Last two years GPA: 3.30/4.00', 
+            'zh_tw': '最後兩年 GPA: 3.30/4.00'
+        }, 
+        'period': {
+            'en': '09 2016 - 07 2020',
+            'zh_tw': '09 2016 - 07 2020',
+        }, 
         'location': {
-            'en': '<em>Hangzhou, China</em>', 
-            'zh_tw': '<em>杭州, 中國</em>'
+            'en': 'Hangzhou, China', 
+            'zh_tw': '杭州, 中國'
         }, 
         'school_hint': {
-            'en': 'Ranked #42 according to QS World University Rankings 2023.', 
-            'zh_tw': '根據QS 2023年世界大學排名，位居 #42 名。'
+            'en': 'Ranked #44 according to QS World University Rankings 2024.', 
+            'zh_tw': '根據 QS 2024 年世界大學排名，位居 #44 名。'
         }, 
         'degree_hint': {
-            'en': 'Ranked #72 in Computer Science and Information Systems according to QS World University Rankings by Subject 2022.', 
-            'zh_tw': '根據QS 2022年世界大學科系排名，於Computer Science and Information Systems領域位居 #72 名。'
+            'en': 'Ranked #36 in Computer Science and Information Systems according to QS World University Rankings by Subject 2023.', 
+            'zh_tw': '根據 QS 2023 年世界大學科系排名，於 Computer Science and Information Systems 領域位居 #36 名。'
         }
     };
     const education_2 = {
@@ -173,24 +247,28 @@ $(document).ready(() => {
             'zh_tw': '國立陽明交通大學'
         }, 
         'degree': {
-            'en': '<em>Master of Science in Computer Science and Engineering</em>', 
-            'zh_tw': '<em>資訊科學與工程研究所甲組碩士</em>'
+            'en': 'Master of Science in Computer Science and Engineering',  
+            'zh_tw': '資訊科學與工程研究所甲組碩士'
         },
         'remark': {
-            'en': '', 
-            'zh_tw': ''
-        },
+            'en': 'First year GPA: 4.04/4.30', 
+            'zh_tw': '第一年 GPA: 4.04/4.30'
+        }, 
+        'period': {
+            'en': '09 2022 - 08 2024 (Expected)',
+            'zh_tw': '09 2022 - 08 2024 (Expected)',
+        }, 
         'location': {
-            'en': '<em>Hsinchu, Taiwan</em>', 
-            'zh_tw': '<em>新竹, 台灣</em>'
+            'en': 'Hsinchu, Taiwan', 
+            'zh_tw': '新竹, 台灣'
         }, 
         'school_hint': {
-            'en': '', 
-            'zh_tw': ''
+            'en': 'Ranked #217 according to QS World University Rankings 2024.', 
+            'zh_tw': '根據 QS 2024 年世界大學排名，位居 #217 名。'
         }, 
         'degree_hint': {
-            'en': '', 
-            'zh_tw': ''
+            'en': 'Ranked #151-200 in Computer Science and Information Systems according to QS World University Rankings by Subject 2023.', 
+            'zh_tw': '根據 QS 2023 年世界大學科系排名，於 Computer Science and Information Systems 領域位居 #151-200 名。'
         }
     };
     const educations = [
@@ -202,16 +280,16 @@ $(document).ready(() => {
      * Captions
      */
     const caption_1 = {
-        'en': 'First prize certificate from CCCC-Mobile Application Innovation Contest.', 
-        'zh_tw': '中國高校計算機大賽移動應用創新賽一等獎獎狀。'
+        'en': 'First Prize certificate from CCCC-Mobile Application Innovation Contest.', 
+        'zh_tw': '中國高校計算機大賽移動應用創新賽 "一等獎" 獎狀。'
     }
     const caption_2 = {
-        'en': 'First prize trophy from CCCC-Mobile Application Innovation Contest.', 
-        'zh_tw': '中國高校計算機大賽移動應用創新賽一等獎獎盃。'
+        'en': 'First Prize and Most Innovative Award trophies from CCCC-Mobile Application Innovation Contest.', 
+        'zh_tw': '中國高校計算機大賽移動應用創新賽 "一等獎" 及 "最具創新獎" 獎盃。'
     }
     const caption_3 = {
-        'en': 'Gold award certificate from The 4th "Internet+" Innovation and Entrepreneurship Competition.', 
-        'zh_tw': '第四屆中國"互聯網+"大學生創新創業大賽金獎獎狀。'
+        'en': 'Gold Award certificate from The 4th "Internet+" Innovation and Entrepreneurship Competition.', 
+        'zh_tw': '第四屆中國"互聯網+"大學生創新創業大賽 "金獎" 獎狀。'
     }
     const captions = [
         caption_1, caption_2, caption_3
@@ -227,19 +305,21 @@ $(document).ready(() => {
         }, 
         'main': {
             'en': [
-                'Intensive 4-week academic program for 2 courses, including "Algorithms and the World Wide Web" and "Building Modern Web Applications".', 
+                'Included 2 courses, "Algorithms and the World Wide Web" and "Building Modern Web Applications".', 
                 'Collaborated with my teammates to complete our projects as team leader.', 
-                'Experienced the active study atmosphere in UBC, and broadened my horizons.'
             ], 
             'zh_tw': [
-                '高強度的四周課程，包含"Algorithms and the World Wide Web"和"Building Modern Web Applications"。', 
-                '在團隊中擔任組長，並和組員共同完成專案。', 
-                '在這四周感受UBC不同的學習氛圍，拓展視野。'
+                '修讀兩門課程 Algorithms and the World Wide Web 和 Building Modern Web Applications。', 
+                '擔任小組組長，與隊友合作完成小型專案。' 
             ]
         }, 
+        'period': {
+            'en': '07 2019 - 08 2019',
+            'zh_tw': '07 2019 - 08 2019',
+        }, 
         'location': {
-            'en': '<em>Vancouver, Canada</em>', 
-            'zh_tw': '<em>溫哥華, 加拿大</em>'
+            'en': 'Vancouver, Canada', 
+            'zh_tw': '溫哥華, 加拿大'
         }
     };
     const experiences = [
@@ -263,7 +343,14 @@ $(document).ready(() => {
              */
             const location_lbl = $('#project-location-' + (idx + 1));
             const location = project['location'][lang];
-            location_lbl.html(location);
+            location_lbl.html(`<em>${location}</em>`);
+
+            /*
+             * Translate period.
+             */
+            const period_lbl = $('#project-period-' + (idx + 1));
+            const period = project['period'][lang];
+            period_lbl.html(`<strong>${period}</strong>`);
 
             /*
              * Translate main.
@@ -316,11 +403,18 @@ $(document).ready(() => {
             prize_lbl.html(prize);
 
             /*
+             * Translate period.
+             */
+            const period_lbl = $('#award-period-' + (idx + 1));
+            const period = award['period'][lang];
+            period_lbl.html(`<strong>${period}</strong>`);
+
+            /*
              * Translate location.
              */
             const location_lbl = $('#award-location-' + (idx + 1));
             const location = award['location'][lang];
-            location_lbl.html(location);
+            location_lbl.html(`<em>${location}</em>`);
         });
 
         /*
@@ -353,11 +447,18 @@ $(document).ready(() => {
             remark_lbl.html(remark);
 
             /*
+             * Translate period.
+             */
+            const period_lbl = $('#education-period-' + (idx + 1));
+            const period = education['period'][lang];
+            period_lbl.html(`<strong>${period}</strong>`);
+
+            /*
              * Translate location.
              */
             const location_lbl = $('#education-location-' + (idx + 1));
             const location = education['location'][lang];
-            location_lbl.html(location);
+            location_lbl.html(`<em>${location}</em>`);
         });
 
         /*
@@ -372,11 +473,18 @@ $(document).ready(() => {
             title_lbl.html(title);
 
             /*
+             * Translate period.
+             */
+            const period_lbl = $('#experience-period-' + (idx + 1));
+            const period = experience['period'][lang];
+            period_lbl.html(`<strong>${period}</strong>`);
+
+            /*
              * Translate location.
              */
             const location_lbl = $('#experience-location-' + (idx + 1));
             const location = experience['location'][lang];
-            location_lbl.html(location);
+            location_lbl.html(`<em>${location}</em>`);
 
             /*
              * Translate main.
